98f5f8d6a8a22773cca24de0d483e1677e9107b2
Coco <coco2008@github.com>
2020-05-03 19:34:54 +0200

    game over

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class EventManager : MonoBehaviour
 {
     public event Action PlayerDied;
+    public event Action RestartGame;
 
     public void OnPlayerDied()
     {
         PlayerDied();
+    }
+
+    public void OnRestartGame()
+    {
+        RestartGame();
     }
 }
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,15 +6,28 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public EventManager eventManager;
+    public GameOverPanelController gameOverPanelController;
+
+    private float _oldTimeScale;
 
     // Start is called before the first frame update
     void Start()
     {
         eventManager.PlayerDied += OnPlayerDied;
+        eventManager.RestartGame += OnRestartGame;
+        gameOverPanelController.gameObject.SetActive(false);
     }
 
     public void OnPlayerDied()
     {
+        _oldTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        gameOverPanelController.gameObject.SetActive(true);
+    }
+
+    public void OnRestartGame()
+    {
+        Time.timeScale = _oldTimeScale;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
